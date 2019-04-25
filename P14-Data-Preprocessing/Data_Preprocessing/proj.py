b5c1c4a1d1813a6fd8a45d70577cc15fb601af2a
A.H.M. Annur <niloyman01@gmail.com>
2019-04-25 19:48:49 +0600

    Add files via upload

---
+++ P14-Data-Preprocessing/Data_Preprocessing/proj.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset = pd.read_csv('Data.csv')
